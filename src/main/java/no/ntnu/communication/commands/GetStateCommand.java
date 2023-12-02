3b3efd9e0fdc678e3e283ead80166b1f2c7d441d
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2023-12-02 16:36:26 +0100

    Revert "Implemented GetStateCommand and Toggle Command"
    This reverts commit bed341e962f857955753043272625d453d2d4fe2.

--- src/main/java/no/ntnu/communication/commands/GetStateCommand.java
+++ src/main/java/no/ntnu/communication/commands/GetStateCommand.java
@@ -1,30 +1,14 @@
 package no.ntnu.communication.commands;
 
-import no.ntnu.communication.Command;
 import no.ntnu.communication.Message;
-import no.ntnu.communication.messages.StateMessage;
-import no.ntnu.greenhouse.Actuator;
 
-/**
- * Command class for getting the state of an Actuator.
- */
-public class GetStateCommand extends Command {
-  private String nodeID;
-  private String actuatorID;
-
+public class GetStateCommand extends Message {
   public GetStateCommand(String nodeID, String actuatorID) {
-    this.nodeID = nodeID;
-    this.actuatorID = actuatorID;
-  }
-
-  @Override
-  public Message execute() {
-    Actuator actuator = new Actuator(actuatorID, Integer.parseInt(nodeID));
-    return new StateMessage(nodeID, actuatorID, actuator.isOn() ? "on" : "off");
+    super();
   }
 
   @Override
   public String messageAsString() {
-    return "GetStateCommand: NodeId=" + nodeID + ", ActuatorID=" + actuatorID;
+    return null;
   }
 }
