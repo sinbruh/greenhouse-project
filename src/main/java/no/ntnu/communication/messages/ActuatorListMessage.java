91463c0dc5168305071ba64e302585f0eb16381c
mathi <mathiaswestergaardjohansen@gmail.com>
2023-12-02 17:28:38 +0100

    Finished ActuatorListMessage class

--- src/main/java/no/ntnu/communication/messages/ActuatorListMessage.java
+++ src/main/java/no/ntnu/communication/messages/ActuatorListMessage.java
@@ -3,13 +3,19 @@ package no.ntnu.communication.messages;
 import no.ntnu.communication.Message;
 
 public class ActuatorListMessage extends Message {
+  Integer[] nodeIDs;
   public ActuatorListMessage(String listOfMessages, String nodeID){
     super();
-
   }
 
   @Override
   public String messageAsString() {
-    return null;
+    StringBuilder builder = new StringBuilder();
+    builder.append("nodes");
+    for (Integer nodeID : nodeIDs) {
+      builder.append("|");
+      builder.append(nodeID);
+    }
+    return builder.toString();
   }
 }
