51a9df07efc62cf5059fc28221c83333056ffbae
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2023-12-03 17:27:10 +0100

    Updated ListofNodesmessage, and removed the commented part in messageserializer

--- src/main/java/no/ntnu/communication/messages/ListOfNodesMessage.java
+++ src/main/java/no/ntnu/communication/messages/ListOfNodesMessage.java
@@ -3,16 +3,21 @@ package no.ntnu.communication.messages;
 import no.ntnu.communication.Message;
 
 public class ListOfNodesMessage extends Message {
-  Integer[] nodeIDs;
-  public ListOfNodesMessage(Integer[] nodeIDs) {
+  String listOfMessages;
+  String nodeIDs;
+
+  public ListOfNodesMessage(String listOfMessages, String nodeIDs) {
     super();
+    this.listOfMessages = listOfMessages;
     this.nodeIDs = nodeIDs;
   }
+
   @Override
   public String messageAsString() {
     StringBuilder builder = new StringBuilder();
-    builder.append("nodes");
-    for (Integer nodeID : nodeIDs) {
+    builder.append(listOfMessages);
+    String[] nodeIDsArray = nodeIDs.split(",");
+    for (String nodeID : nodeIDsArray) {
       builder.append("|");
       builder.append(nodeID);
     }
