0a9cb9f71baa2087e6a7caddd47d9bf6d3c4f6c4
mathi <mathiaswestergaardjohansen@gmail.com>
2023-12-04 15:26:47 +0100

    refactored MessageSerializer based on setStateCommand

--- src/main/java/no/ntnu/communication/MessageSerializer.java
+++ src/main/java/no/ntnu/communication/MessageSerializer.java
@@ -14,15 +14,13 @@ public class MessageSerializer {
     String[] tokens = rawClientRequest.split("\\|");
     switch (tokens[0]) {
       //Commands
-      case "on" -> message = new TurnOnCommand(tokens[0], tokens[1], tokens[2]);
-      case "off" -> message = new TurnOffCommand(tokens[0], tokens[1], tokens[2]);
-      case "toggle" -> message = new Toggle(tokens[0], tokens[1]);
+      case "setState" -> message = new SetStateCommand(tokens[0], tokens[1], tokens[2]);
       case "getState" -> message = new GetStateCommand(tokens[0], tokens[1]);
       case "getSensors" -> message = new GetListOfSensors(tokens[0]);
       case "getActuators" -> message = new GetListOfActuators(tokens[0]);
       case "getNodes" -> message = new GetListOfNodes();
       case "getValue" -> message = new GetValueCommand(tokens[0], tokens[1]);
-      case "ready" -> message = new ReadyCommand();
+      //case "ready" -> message = new ReadyCommand();
       //Messages
       case "state" -> message = new StateMessage(tokens[0], tokens[1], tokens[2]);
       case "sensors" -> message = new SensorListMessage(tokens[0], tokens[1]);
