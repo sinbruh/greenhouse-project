d886245f4e271fab660ae55fd75ff3e368d3476c
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2023-12-04 13:51:49 +0100

    Added JavaDoc to MessageSerializer

--- src/main/java/no/ntnu/communication/MessageSerializer.java
+++ src/main/java/no/ntnu/communication/MessageSerializer.java
@@ -9,6 +9,12 @@ import no.ntnu.communication.messages.StateMessage;
 import no.ntnu.communication.messages.TemperatureMessage;
 
 public class MessageSerializer {
+
+  /**
+   * Deserializes a string into a Message object. The type of the Message object is determined by the first token in the string.
+   * @param messageString The string to deserialize.
+   * @return The deserialized Message object, or null if the string could not be deserialized.
+   */
   public static Message fromString(String rawClientRequest) {
     Message message = null;
     String[] tokens = rawClientRequest.split("\\|");
@@ -33,6 +39,11 @@ public class MessageSerializer {
     return message;
   }
 
+  /**
+   * Serializes a Message object into a string.
+   * @param response The Message object to serialize.
+   * @return The serialized Message object as a string.
+   */
   public static String toString(Message response) {
     return response.messageAsString();
   }
