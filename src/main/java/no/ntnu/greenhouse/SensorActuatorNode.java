f1ac76c3610461b347c98457f1713c77040461a3
Sten <stenhalse@gmail.com>
2023-12-09 16:11:15 +0100

    Sonarlint changes

--- src/main/java/no/ntnu/greenhouse/SensorActuatorNode.java
+++ src/main/java/no/ntnu/greenhouse/SensorActuatorNode.java
@@ -2,7 +2,6 @@ package no.ntnu.greenhouse;
 
 import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
